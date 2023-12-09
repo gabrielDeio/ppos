9db74cfd0785e13043dca8c890b59d2316e2d2d4
gabriel_lira <gabrieldeio40@gmail.com>
2023-12-09 04:19:08 -0300

    fix: test 2

--- ppos_disk.c
+++ ppos_disk.c
@@ -37,6 +37,10 @@ void fcsc_body(void *arg){
 int disk_mgr_init (int *numBlocks, int *blockSize){
     disk = malloc(sizeof(disk_t));
     disk->task_queue = NULL;
+    //disk->queue_mutex = malloc(sizeof(mutex_t));
+    //disk->disk_mutex = malloc(sizeof(mutex_t));
+    disk->queue_sem = malloc(sizeof(disk->queue_sem));
+    disk->disk_sem = malloc(sizeof(disk->disk_sem));
 
     usrsig.sa_handler = handler;
     sigemptyset(&usrsig.sa_mask);
@@ -51,13 +55,44 @@ int disk_mgr_init (int *numBlocks, int *blockSize){
     perror("Erro em criacao de disco");
     exit(1);
     }
+    /*
+    
+            if(mutex_create(disk->queue_mutex)){
+        perror("Error em criacao de mutex");
+        exit(1);
+    }
+    
+    if(mutex_create(disk->disk_mutex)){
+        perror("Error em criacao de mutex");
+        exit(1);
+    }
+    */
+    
+    
 
+    
+    if(sem_create(disk->queue_sem, 1)){
+        perror("Error em criacao de semaforo");
+        exit(1);
+    }
+
+    
+    if(sem_create(disk->disk_sem, 1)){
+        perror("Error em criacao de semaforo");
+        exit(1);
+    }
+    
+
+    
+    
     printf("\n%d\n", task_create(&disk->scheduler, fcsc_body, ""));
 
     return 0;
 }
 
 int disk_block_read(int block, void* buffer){
+    //mutex_lock(disk->queue_mutex);
+    sem_down(disk->queue_sem);
     waiting *t = malloc(sizeof(waiting));
 
     t->operation = DISK_CMD_READ;
@@ -68,19 +103,26 @@ int disk_block_read(int block, void* buffer){
     t->task->state = PPOS_TASK_STATE_SUSPENDED;
 
     waiting *aux = disk->task_queue;
-    if(disk->task_queue == NULL) disk->task_queue = t;
+     if(disk->task_queue == NULL) {
+        disk->task_queue = t;
+        sem_up(disk->queue_sem);
+        //mutex_unlock(disk->queue_mutex);
+    }
     else{
         while(aux->next != NULL){
         aux = aux->next;   
         }
         aux->next = t;
+        sem_up(disk->queue_sem);
+        //mutex_unlock(disk->queue_mutex);
     }
     
     task_suspend(t->task, &sleepQueue);
     task_yield();
     
 
-    
+    //mutex_lock(disk->disk_mutex);
+    sem_down(disk->disk_sem);
     disk->ready_task_queue = t;
     
     disk_cmd(t->operation, t->block, t->buffer);
@@ -92,12 +134,16 @@ int disk_block_read(int block, void* buffer){
     while (disk_cmd(DISK_CMD_STATUS, 0, 0) != DISK_STATUS_IDLE)
     {
     }
+    sem_up(disk->disk_sem);
+    //mutex_unlock(disk->disk_mutex);
 
     return 0;
 }
 
 int disk_block_write(int block, void* buffer){
-     waiting *t = malloc(sizeof(waiting));
+    //mutex_lock(disk->queue_mutex);
+    sem_down(disk->queue_sem);
+    waiting *t = malloc(sizeof(waiting));
 
     t->operation = DISK_CMD_WRITE;
     t->block = block;
@@ -107,19 +153,26 @@ int disk_block_write(int block, void* buffer){
     t->task->state = PPOS_TASK_STATE_SUSPENDED;
 
     waiting *aux = disk->task_queue;
-    if(disk->task_queue == NULL) disk->task_queue = t;
+     if(disk->task_queue == NULL) {
+        disk->task_queue = t;
+        sem_up(disk->queue_sem);
+        //mutex_unlock(disk->queue_mutex);
+    }
     else{
         while(aux->next != NULL){
         aux = aux->next;   
         }
         aux->next = t;
+        sem_up(disk->queue_sem);
+        //mutex_unlock(disk->queue_mutex);
     }
     
     task_suspend(t->task, &sleepQueue);
     task_yield();
     
 
-    
+    //mutex_lock(disk->disk_mutex);
+    sem_down(disk->disk_sem);
     disk->ready_task_queue = t;
     
     disk_cmd(t->operation, t->block, t->buffer);
@@ -131,6 +184,8 @@ int disk_block_write(int block, void* buffer){
     while (disk_cmd(DISK_CMD_STATUS, 0, 0) != DISK_STATUS_IDLE)
     {
     }
+    sem_up(disk->disk_sem);
+    //mutex_unlock(disk->disk_mutex);
 
     return 0;
 }
