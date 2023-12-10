42659bac922201f1e61e547dd5180ee26617d107
gabriel_lira <gabrieldeio40@gmail.com>
2023-12-10 15:26:25 -0300

    feat: implements cscan

--- new_ppos_disk.c
+++ new_ppos_disk.c
@@ -6,20 +6,20 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
+#include <unistd.h>
 
 struct sigaction usrsig;
 disk_t* disk;
 
 void handler(int signum){
-    //printf("caiu no handler\n");
-    //sem_down(disk->handle_sem);
-    disk->task_queue->status = 1;
     disk->scheduler.state = PPOS_TASK_STATE_READY; 
     task_resume(&disk->scheduler);
+    disk->status = 1;
+    
 }
 
-disk_tasks* create_task(task_t *current_task ,int operation, int block, void *buffer){
-    disk_tasks *task = malloc(sizeof(disk_tasks));
+disk_task* create_task(task_t *current_task ,int operation, int block, void *buffer){
+    disk_task *task = malloc(sizeof(disk_task));
 
     task->operation = operation;
     task->block = block;
@@ -31,29 +31,32 @@ disk_tasks* create_task(task_t *current_task ,int operation, int block, void *bu
     return task;
 }
 
-void disk_tasks_append(disk_tasks *task){
+void disk_task_append(disk_task *task){
+    
     if(disk->task_queue == NULL){
         //printf("\nEmpty Queue\n");
         disk->task_queue = task;
         return;
     }
 
-    disk_tasks *aux = disk->task_queue;
+    disk_task *aux = disk->task_queue;
     while(aux->next != NULL){
         aux = aux->next;
     }
 
     aux->next = task;
+    
+    return;
 }
 
-disk_tasks* disk_tasks_remove(){
-    if(disk->task_queue == NULL){
+disk_task* disk_task_remove(){
+    if(disk->scheduler->task->queue == NULL){
         printf("\nEmpty Queue\n");
         return NULL;
     }
 
-    disk_tasks *aux = disk->task_queue;
-    disk->task_queue = aux->next;
+    disk_task *aux = disk->scheduler->task->queue;
+    disk->scheduler->task->queue = aux->next;
     aux->next = NULL;
 
     return aux;
@@ -61,27 +64,25 @@ disk_tasks* disk_tasks_remove(){
 
 
 void fcsc_body(){
-    while(sleepQueue != NULL || readyQueue != NULL){
-        sem_down(disk->queue_sem);
-
-        if(disk->task_queue->status != 0) {
-            disk_tasks *task_to_finish = disk_tasks_remove();
-            task_to_finish->task->state = PPOS_TASK_STATE_READY;
-            //sem_up(disk->handle_sem);
-            task_resume(task_to_finish->task);
+    while(1){
+        printf("passou no body\n");
+        sem_down(disk->disk_sem);
+        if(disk->status != 0) {
+            disk_task *task_to_finish = disk_task_remove();
+            //task_to_finish->task->state = PPOS_TASK_STATE_READY;
+            task_resume(task_to_finish);
+            disk->status = 0;
         }
 
-        if(disk_cmd(DISK_CMD_STATUS, 0, 0) == DISK_STATUS_IDLE && disk->task_queue != NULL){
-            //printf("caiu no comando : %d\n", disk_cmd(DISK_CMD_STATUS, 0, 0));
-            disk_cmd(disk->task_queue->operation, disk->task_queue->block, disk->task_queue->buffer);
-             while (disk_cmd(DISK_CMD_STATUS, 0, 0) != DISK_STATUS_IDLE)
-                {
-                }
+        if(disk_cmd(DISK_CMD_STATUS, 0, 0) == DISK_STATUS_IDLE && disk->scheduler->task->queue != NULL){
+            printf("requisitou\n");
+            disk_cmd(disk->scheduler->task->queue->operation, disk->scheduler->task->queue->block, disk->scheduler->task->queue->buffer);
         }
-
-        sem_up(disk->queue_sem);
+        
+        //sem_up(disk->queue_sem);
         disk->scheduler.state = PPOS_TASK_STATE_SUSPENDED;
         task_suspend(&disk->scheduler, &sleepQueue);
+        sem_up(disk->disk_sem);
         task_yield();
     }
 }
@@ -92,12 +93,12 @@ int disk_mgr_init (int *numBlocks, int *blockSize){
     disk = malloc(sizeof(disk_t));
     
     disk->task_queue = NULL;
+
     
     disk->queue_sem = malloc(sizeof(semaphore_t));
-    //disk->handle_sem = malloc(sizeof(semaphore_t));
-    //disk->scheduler = malloc(sizeof(task_t));
-    
-    //disk->handle_count = 0;
+    disk->disk_sem = malloc(sizeof(semaphore_t));
+    //disk->handler_sem = malloc(sizeof(semaphore_t));
+    disk->status = 0;
 
     usrsig.sa_handler = handler;
     sigemptyset(&usrsig.sa_mask);
@@ -115,21 +116,18 @@ int disk_mgr_init (int *numBlocks, int *blockSize){
 
     
 
-    if(sem_create(disk->queue_sem, 1)){
-        perror("Erro em criacao de semaforo da fila");
+   if(sem_create(disk->queue_sem, 1)){
+        perror("Error em criacao de semaforo");
         exit(1);
     }
 
     
+    if(sem_create(disk->disk_sem, 1)){
+        perror("Error em criacao de semaforo");
+        exit(1);
+    }
 
-    //if(sem_create(disk->handle_sem, 1)){
-        //perror("Error em criacao de semaforo do handle");
-        //exit(1);
-    //}
-
-
-
-    task_create(&disk->scheduler, fcsc_body, "");
+    printf("\n%d\n", task_create(&disk->scheduler, fcsc_body, ""));
 
     return 0;
 }
@@ -137,20 +135,18 @@ int disk_mgr_init (int *numBlocks, int *blockSize){
 
 int disk_block_read(int block, void* buffer){
     sem_down(disk->queue_sem);
+    disk_task *task = create_task(taskExec, DISK_CMD_READ, block, buffer);
 
-    disk_tasks *task = create_task(taskExec, DISK_CMD_READ, block, buffer);
-
-    disk_tasks_append(task);
-
+    disk_task_append(task);
+    
     if(disk->scheduler.state == PPOS_TASK_STATE_SUSPENDED){
         disk->scheduler.state = PPOS_TASK_STATE_READY;
         task_resume(&disk->scheduler);
     }
 
+    task->task->state = PPOS_TASK_STATE_SUSPENDED;
+    task_suspend(task->task, disk->scheduler->queue);
     sem_up(disk->queue_sem);
-    
-    task->status = PPOS_TASK_STATE_SUSPENDED;
-    task_suspend(task->task, &sleepQueue);
     task_yield();
 
     return 0;
@@ -158,20 +154,18 @@ int disk_block_read(int block, void* buffer){
 
 int disk_block_write(int block, void* buffer){
     sem_down(disk->queue_sem);
+    disk_task *task = create_task(taskExec, DISK_CMD_WRITE, block, buffer);
 
-    disk_tasks *task = create_task(taskExec, DISK_CMD_WRITE, block, buffer);
-
-    disk_tasks_append(task);
+    disk_task_append(task);
 
     if(disk->scheduler.state == PPOS_TASK_STATE_SUSPENDED){
         disk->scheduler.state = PPOS_TASK_STATE_READY;
         task_resume(&disk->scheduler);
     }
-
-    sem_up(disk->queue_sem);
     
-    task->status = PPOS_TASK_STATE_SUSPENDED;
-    task_suspend(task->task, &sleepQueue);
+    task->task->state = PPOS_TASK_STATE_SUSPENDED;
+    task_suspend(task->task, disk->scheduler->queue);
+    sem_up(disk->queue_sem);
     task_yield();
 
     return 0;
